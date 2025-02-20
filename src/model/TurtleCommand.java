64cae604643e83c548f55c5b05528dd1ec42608f
EliyasT <eliyasbirhanu1991@gmail.com>
2025-02-20 18:29:14 +0100

    renamed command name and changed characters

--- src/model/TurtleCommand.java
+++ src/model/TurtleCommand.java
@@ -6,11 +6,11 @@ package model;
 import java.util.Map;
 
 public enum TurtleCommand {
-	MOVE, TURNRIGHT, TURNLEFT, SAVE, REMOVE;
+	MOVE, TURNRIGHT, TURNLEFT, PUSH, POP;
 
-	public static final Map<Character, TurtleCommand> PREDEFINED_COMMANDS = Map.of('F', TurtleCommand.MOVE, 'G',
-			TurtleCommand.MOVE, 'V', TurtleCommand.MOVE, 'X', TurtleCommand.MOVE, 'Y', TurtleCommand.MOVE, 'Z',
-			TurtleCommand.MOVE, '+', TurtleCommand.TURNRIGHT, '-', TurtleCommand.TURNLEFT, '[', TurtleCommand.SAVE, ']',
-			TurtleCommand.REMOVE);
+	public static final Map<Character, TurtleCommand> PREDEFINED_COMMANDS = Map.of('A', TurtleCommand.MOVE, 'B',
+			TurtleCommand.MOVE, 'F', TurtleCommand.MOVE, 'G', TurtleCommand.MOVE, 'X', TurtleCommand.MOVE, 'Y',
+			TurtleCommand.MOVE, '+', TurtleCommand.TURNRIGHT, '-', TurtleCommand.TURNLEFT, '[', TurtleCommand.PUSH, ']',
+			TurtleCommand.POP);
 
 }
