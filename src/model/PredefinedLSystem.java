d8e62e08f0c57d73b84528872ea5e8dac92497d2
EliyasT <eliyasbirhanu1991@gmail.com>
2025-02-24 11:44:58 +0100

    Added sortd array method  to sort the array by name

--- src/model/PredefinedLSystem.java
+++ src/model/PredefinedLSystem.java
@@ -3,6 +3,9 @@
  */
 package model;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class PredefinedLSystem {
 	private final String name;
 	private final String axiom;
@@ -47,20 +50,19 @@ public class PredefinedLSystem {
 
 	// Predefined LSystems
 	public static PredefinedLSystem[] predefinedLSystems() {
-		return new PredefinedLSystem[] { 
+		PredefinedLSystem[] predefined = {
 				new PredefinedLSystem("Quadratic Snowflake", "F-F-F-F", "F->F+F-F-F+F", 90, 5, 4),
-				new PredefinedLSystem("Sierpinski Triangle", "F+X+X", "F->F+X-F-X+F, X->XX", 120, 5, 5),
 				new PredefinedLSystem("Dragon Curve", "FX", "X->X+YF+, Y->-FX-Y", 90, 10, 10),
-				new PredefinedLSystem("L-System Bush1", "Y", "X->X[-FFF][+FFF]FX, Y->YFX[+Y][-Y]", 25.7, 5, 5),
-				new PredefinedLSystem("Simple barnching plant", "X", "X->F[-X][X]F[-X]+FX,F->FF", 20, 5, 5),
+				new PredefinedLSystem("Weed", "Y", "X->X[-FFF][+FFF]FX, Y->YFX[+Y][-Y]", 25.7, 5, 5),
+				new PredefinedLSystem("Plant3", "X", "X->F[-X][X]F[-X]+FX,F->FF", 20, 5, 5),
 				new PredefinedLSystem("Seaweed", "F", "F->FF+[+F-F-F]-[-F+F+F]", 22.5, 7, 4),
 				new PredefinedLSystem("Stick Plant", "X", "X->F[+X][-X]FX, F->FF[-F]F", 20, 6, 4),
 				new PredefinedLSystem("Weed Plant", "X", "X->F[+X][-X]FX, F->FF", 25, 10, 5),
-				new PredefinedLSystem("Simple plant", "X", "F->FF, X->F[+X]F[-X]+X", 20, 5, 6),
-				new PredefinedLSystem("Flower Like Plant", "F", "F->F[+F][-F]", 15, 10, 6),
-				new PredefinedLSystem("Plant", "X", "F->FF, X->F+[[FX]-X]-F[-FX]+X", 20, 8, 5),
-				new PredefinedLSystem("Tree", "X", "F->FF, X->F[+X][-X]", 45, 10, 6),
-				new PredefinedLSystem("Lévy curve", "F", "F->-F++F-", 45, 5, 4),
+				new PredefinedLSystem("Plant1", "X", "F->FF, X->F[+X]F[-X]+X", 20, 5, 6),
+				new PredefinedLSystem("Tree1", "F", "F->F[+F][-F]", 15, 10, 6),
+				new PredefinedLSystem("Plant2", "X", "F->FF, X->F+[[FX]-X]-F[-FX]+X", 20, 8, 5),
+				new PredefinedLSystem("Tree2", "X", "F->FF, X->F[+X][-X]", 45, 10, 6),
+				new PredefinedLSystem("Lévy curve", "F", "F->-F++F-", 45, 5, 10),
 				new PredefinedLSystem("Rings", "F+F+F+F", "F->FF+F+F+F+F+F-F", 90, 5, 4),
 				new PredefinedLSystem("Cross", "F+F+F+F", "F->F+F-F+F+F", 90, 5, 4),
 				new PredefinedLSystem("Koch Snowflake", "F++F++F", "F->F-F++F-F", 60, 5, 4),
@@ -70,5 +72,9 @@ public class PredefinedLSystem {
 				new PredefinedLSystem("Quadratic Koch Island", "F+F+F+F", "F->F+F-F-FFF+F+F-F", 90, 5, 4),
 				new PredefinedLSystem("Crystal", "F+F+F+F", "F->FF+F++F+F", 90, 5, 4),
 				new PredefinedLSystem("Sierpinski Square", "F+XF+F+XF", "X -> XF-F+F-XF+F+XF-F+F-X", 90, 5, 4) };
+		Arrays.sort(predefined, Comparator.comparing(PredefinedLSystem::getName));
+
+		return predefined;
 	}
+
 }
